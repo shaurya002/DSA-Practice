9a340dd64c8e3ce632bc41a0b55ac259064da4eb
Shaurya Pratap Singh <90743951+shaurya002@users.noreply.github.com>
2025-04-21 23:26:10 +0530

    Update 0033-search-in-rotated-sorted-array.java

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -6,13 +6,14 @@ class Solution {
             int m = s+(e-s)/2;
             if(arr[m]==target){
                 return m;
-            }
+            } //left sorted check....
             if(arr[s]<=arr[m]){
                 if(target>=arr[s] && target<=arr[m]){
                     e = m - 1;
                 }else{
                     s = m+1;
                 }
+                //if not left sorted then its definately right sorted... 
             }else{
                 if(arr[e]>=target && target>=arr[m]){
                     s = m+1;
